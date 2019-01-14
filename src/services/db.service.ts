3fd266a6fdbd8550139c6d7a100512d3087f76e1
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-01-14 23:24:40 +0300

    add db service

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DbService {
-
-  constructor() { }
+  constructor(public afs: AngularFirestore) {
+    console.log("db service constructor", afs);
+  }
 }
+
+// NOTE - could force offline-first approach with
+// https://firebase.google.com/docs/firestore/manage-data/enable-offline
