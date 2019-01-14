6b165ef87f72727c554a5edde642029e5a517e78
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-01-14 11:30:36 +0300

    update folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
   },
   {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
+    path: "home",
+    loadChildren: "pages/home/home.module#HomePageModule"
   },
   {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+    path: "list",
+    loadChildren: "pages/list/list.module#ListPageModule"
+  },
+  { path: "edit", loadChildren: "pages/edit/edit.module#EditPageModule" }
 ];
 
 @NgModule({
