bca8ff3c853e99ffcda33cee91e27fd5491ae3a6
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-02-10 01:01:54 +0000

    fix sw update (closes #52 closes #40)

--- src/components/general-components/general.components.module.ts
+++ src/components/general-components/general.components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 import { ProfileButtonComponent } from "./profile-button/profile-button.component";
+import { SWCheckComponent } from "./sw-check";
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule],
-  declarations: [ProfileButtonComponent],
-  exports: [ProfileButtonComponent]
+  declarations: [ProfileButtonComponent, SWCheckComponent],
+  exports: [ProfileButtonComponent, SWCheckComponent]
 })
 export class GeneralComponentsModule {}
